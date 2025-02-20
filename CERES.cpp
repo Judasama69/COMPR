054c7cb39118afd0c568ee4a89235d5e875c2089
Judasama69 <kenjiepen14@gmail.com>
2025-02-20 21:10:03 +0800

    commit

--- CERES.cpp
+++ CERES.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     string destinationName;
-    int destination, userType;
+    int destination, userTyped;
     float total, fare, discount = 0;
 
     cout << "1. Bais - Dumaguete (80 Pesos)\n";
@@ -14,38 +14,31 @@ int main() {
     cout << "Where to stop? (Choose 1 - 5): ";
     cin >> destination;
 
-     switch (destination) {
+    switch (destination) {
         case 1: fare = 80; destinationName = "Bais to Dumaguete"; break;
         case 2: fare = 70; destinationName = "Bais to Sibulan"; break;
         case 3: fare = 60; destinationName = "Bais to San Jose"; break;
         case 4: fare = 50; destinationName = "Bais to Amlan"; break;
         case 5: fare = 40; destinationName = "Bais to Tanjay"; break;
-        default: 
-            cout << "Invalid choice!";
-            return 1;
+        default: cout << "Invalid choice!\n"; 
+		return main();
     }
 
     cout << "\n1. Student? (5% Discount)";
     cout << "\n2. Senior? (10% Discount)";
     cout << "\n3. Regular? (No Discount)";
-    cout << "\nChoose and press 1, 2, or 3: ";
-    cin >> userType;
+    cout << "\n(Choose your eligibility (1,2,3) ): ";
+    cin >> userTyped;
     cout << "\nYour destination is " << destinationName;
 
-    if (userType == 1) {
-        discount = fare * 0.05;
-        cout << "\nYou are a Student and your discount is " << discount << " Pesos" << endl;
-    } else if (userType == 2) {
-        discount = fare * 0.10;
-        cout << "\nYou are a Senior and your discount is " << discount << " Pesos" << endl;
-    } else if (userType == 3) {
-        discount = 0;
-        cout << "\nYou are a Regular passenger and have no discount." << endl;
-    } else {
-        cout << "Invalid choice!";
-        return 1;
+    switch (userTyped) {
+        case 1: discount = fare * 0.05; cout << "\nYou are a Student and your discount is " << discount << " Pesos" << endl; break;
+        case 2: discount = fare * 0.10; cout << "\nYou are a Senior and your discount is " << discount << " Pesos" << endl; break;
+        case 3: discount = 0; cout << "\nYou are a Regular passenger and have no discount." << endl; break;
+        default: cout << ", but you made an invalid choice. Please try again.\n";
+		return main();
     }
-	
+
     total = fare - discount;
     cout << "Your total payment is " << total << " Pesos" << endl;
     return 0;
